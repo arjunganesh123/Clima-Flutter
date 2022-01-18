b196ac1e42f6ec6afea0216790e4c94332798fd9
arjunganesh123 <arjunganeshgonibeedu44@gmail.com>
2022-01-18 11:22:02 +0530

    some code to work on network.

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,8 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 class WeatherModel {
+  double longitude;
+  double latitude;
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -19,6 +23,25 @@ class WeatherModel {
     }
   }
 
+  Future<dynamic> getcityweather(dynamic typedname) async{
+     Network net=Network('https://api.openweathermap.org/data/2.5/weather?q=$typedname&appid=8d2ab8285d11966578267a5fb7d497aa&units=metric');
+
+     var data=await net.getdata();
+     return data;
+  }
+
+
+  Future<dynamic> getposition() async{
+    Location location=Location();
+    await location.getposition();
+    longitude=location.longitude;
+    latitude=location.latitude;
+
+    Network network=Network('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=8d2ab8285d11966578267a5fb7d497aa&units=metric');
+
+    var data= await network.getdata();
+    return data;
+  }
   String getMessage(int temp) {
     if (temp > 25) {
       return 'It\'s 🍦 time';
